61579ea80ddc0f03390b6d8807cc4f961ef72d49
salaboy <Salaboy@gmail.com>
2022-09-22 10:14:11 +0100

    updating message

--- go/http/handle.go
+++ go/http/handle.go
@@ -14,5 +14,5 @@ func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
 	 */
 
-	fmt.Fprintf(res, "What a rush! We deploy a function on a keynote!")
+	fmt.Fprintf(res, "\n\nWhat a RUSH!!!! We just deployed a Knative function live on a KEYNOTE!\n\n")
 }
