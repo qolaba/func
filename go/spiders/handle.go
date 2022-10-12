73c949c17dc2ad6b96a9594e3ad03271f9fb21f6
salaboy <Salaboy@gmail.com>
2022-10-12 10:49:59 +0100

    updating template for spiders

--- go/spiders/handle.go
+++ go/spiders/handle.go
@@ -14,7 +14,7 @@ func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
 	 */
 
-	fileBytes, err := ioutil.ReadFile("rainbowsrainbowsrainbows.png")
+	fileBytes, err := ioutil.ReadFile("rainbows-spider.png")
 	if err != nil {
 		panic(err)
 	}
